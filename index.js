96d2794afe30b2c252c90754378c4f36a3b8bf54
Jared Petersen <jaredtoddpetersen@gmail.com>
2017-11-26 14:53:57 -0800

    changed parameter names to match the test

--- index.js
+++ index.js
@@ -13,34 +13,34 @@ module.exports.mock = () => {
 
   // Define the behavior of each of the model functions
 
-  ModelMock.find.returns = (err, value) => {
+  ModelMock.find.returns = (err, docs) => {
     ModelMock.find = (...parameters) => {
       // Callback is always the last argument in the list
       const callback = parameters[parameters.length - 1];
-      callback(err, value);
+      callback(err, docs);
     };
   };
 
-  ModelMock.findById.returns = (err, value) => {
+  ModelMock.findById.returns = (err, doc) => {
     ModelMock.findById = (...parameters) => {
       // Callback is always the last argument in the list
       const callback = parameters[parameters.length - 1];
-      callback(err, value);
+      callback(err, doc);
     };
   };
 
-  ModelMock.prototype.save.returns = (err, value) => {
+  ModelMock.prototype.save.returns = (err, doc) => {
     ModelMock.prototype.save = function (...parameters) {
       // Callback is always the last argument in the list
       const callback = parameters[parameters.length - 1];
 
-      if (err === undefined || value === undefined) {
+      if (err === undefined || doc === undefined) {
         // User did not specify callback values so use the object properties
         callback(null, Object.assign({}, this));
       }
       else {
         // User specified callback values so honor them
-        callback(err, value);
+        callback(err, doc);
       }
     };
   };
